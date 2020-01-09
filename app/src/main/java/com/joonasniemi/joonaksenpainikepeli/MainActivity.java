753a2a7ac8a42d05ca73370aba6a923961e71c4e
jopakka <jopakkamies@gmail.com>
2020-01-09 22:14:54 +0200

    Username and password field added

--- app/src/main/java/com/joonasniemi/joonaksenpainikepeli/MainActivity.java
+++ app/src/main/java/com/joonasniemi/joonaksenpainikepeli/MainActivity.java
@@ -3,13 +3,15 @@ package com.joonasniemi.joonaksenpainikepeli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText etUsername;
     private EditText etPassword;
     private Button bAdd;
@@ -18,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        bAdd = findViewById(R.id.bAdd);
+
+        bAdd.setOnClickListener(this);
     }
 
     @Override
@@ -29,5 +37,12 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.bAdd) {
+            Toast.makeText(this, "Button toimii", Toast.LENGTH_LONG).show();
+        }
     }
 }
